4f87539a470d1c95b162d20b215af254bc95da16
alsxm <tulip23@naver.com>
2021-07-06 04:26:19 +0900

    [studygroup] complete BOJ 1202

--- studygroup/BOJ_1202.py
+++ studygroup/BOJ_1202.py
@@ -1,35 +1,27 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 jewels = []
 bags = []
-check = [False] * K
+
 for _ in range(N):
-    m, v = map(int, input().split())
-    jewels.append((m, v))
+    heapq.heappush(jewels, tuple(map(int, input().split())))
 for _ in range(K):
     bags.append(int(input().rstrip()))
 
-answer = 0
-jewels.sort(key=lambda x: x[1], reverse=True)
 bags.sort()
+answer = 0
+candidates = []
 
-for jewel in jewels:
-    w, v = jewel[0], jewel[1]
-    if w > bags[-1]:
-        continue
-    start, end = 0, K-1
-    while start <= end:
-        mid = (start + end) // 2 
-        if (end - start) == 1 and bags[start] <= w < bags[end] and not check[start]:
-            answer += v
-            check[start] = True
-            break
-        elif w < bags[mid]:
-            end = mid
-        elif w >= bags[mid]:
-            start = mid
-        print(start, end, end=' ')
+for bag in bags:
+    while jewels and bag >= jewels[0][0]:
+        w, v = heapq.heappop(jewels)
+        heapq.heappush(candidates, -v)
+    
+    if candidates:
+        answer -= heapq.heappop(candidates)
+    
 
 print(answer)
